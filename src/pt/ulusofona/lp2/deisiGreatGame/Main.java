f6fad659f73b989fbc9d88b32f1ce2f019534f08
João Eleutério a22007237 <a22007237@alunos.ulht.pt>
2021-10-30 11:19:06 +0100

    adicionei o AUTHORS.txt Mudei o ProgrammerColor Mudei o toString dos Programmer

--- src/pt/ulusofona/lp2/deisiGreatGame/Main.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/Main.java
@@ -1,4 +1,25 @@
 package pt.ulusofona.lp2.deisiGreatGame;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+      /*  HashMap<Integer, Programmer> players1 = new HashMap<>();
+        players1.put(1,new Programmer("aaa",linguagens("aaa;bbb;ccc"),1,ProgrammerColor.getColor("Blue")));
+        players1.put(2,new Programmer("bbb",linguagens("aaa"),3,ProgrammerColor.getColor("Green")));
+        players1.put(3,new Programmer("ccc",linguagens("asdsa;sdasad;asdsada;Adasda"),4,ProgrammerColor.getColor("Purple")));
+
+
+        for (int i=1;i< players1.size()+1;i++){
+            System.out.println(players1.get(i));
+        }*/
+    }
+/*public static ArrayList<String> linguagens(String linguagens){
+        String[] linguagem = linguagens.split(";");
+        return new ArrayList<String>(List.of(linguagem));
+    }*/
+
+
 }
