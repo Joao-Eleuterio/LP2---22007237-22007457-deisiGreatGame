f4fb505512a35550ec5a4cd5a7474d9a6530de03
Mario Silva <a22007457@alunos.ulht.pt>
2021-11-29 21:56:21 +0000

    Acrescentei os cases do String getConsequencia().

--- src/pt/ulusofona/lp2/deisiGreatGame/Ferramenta.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/Ferramenta.java
@@ -16,12 +16,12 @@ public class Ferramenta extends Trap {
 
     public String  getConsequencia(){
         return switch (titulo){
-        case "Herança" ->"aaaa";
-        case "Programação funcional " ->"bbbb";
-        case "Testes unitários" -> "cccc";
-        case "Tratamento de Excepções" -> "ddddd";
-        case "IDE" ->"eeeee";
-        case "Ajuda Do Professor" -> "Anula os abismos de Sintaxe.Lógica,File Not Found e Exception  ";
+        case "Herança" ->"Anula os feitos de codigo duplicado";
+        case "Programação funcional " ->"Duplicated Code, efeitos secundários";
+        case "Testes unitários" -> "Anula os abismos de Sintaxe,Lógica";
+        case "Tratamento de Excepções" -> "Anula os efeitos dos abismos de File Not Found e Exception";
+        case "IDE" ->"Anula os abismos de Sintaxe";
+        case "Ajuda Do Professor" -> "Anula os abismos de Sintaxe,Lógica,File Not Found e Exception";
         default -> null;
     };}
     @Override
