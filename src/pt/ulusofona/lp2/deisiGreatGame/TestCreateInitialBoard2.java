924fe29083c9062e0ba3aa7c88b7a1fed238f137
Mario Silva <a22007457@alunos.ulht.pt>
2021-12-12 21:04:28 +0000

    Adicionei mais alguns testes unitarios.

--- src/pt/ulusofona/lp2/deisiGreatGame/TestCreateInitialBoard2.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/TestCreateInitialBoard2.java
@@ -78,6 +78,20 @@ public class TestCreateInitialBoard2 {
     }
 
 
+    //herença
+    @Test
+    public void test03CreateInitialBoard() {
+
+        pt.ulusofona.lp2.deisiGreatGame.GameManager manager = new pt.ulusofona.lp2.deisiGreatGame.GameManager();
+        manager.createInitialBoard(creat4Players(),25,abyssesAndTools());
+        assertTrue(manager.moveCurrentPlayer(1));
+        String esperada1 = "Caiu Herança! Anula os feitos de codigo duplicado";
+        assertEquals(esperada1,manager.reactToAbyssOrTool());
+
+
+    }
+
+
 
 
 
